c90181b726a43e894e87107bb41e7d5c5e41e4c4
Jim <jimsamuel50@gmail.com>
2025-01-19 08:41:58 +0100

    Lkvs.WAL:Fault tolerance recovery. replay WAL file contents for memtable. sstable again

--- pkg/model/wal.go
+++ pkg/model/wal.go
@@ -1,11 +1,14 @@
 package model
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os"
 )
 
 var (
+	ErrOpeningFile  = "could not open write ahead log file"
 	ErrFileProblems = "could not create or append data to files"
 	ErrGenericWAL   = "write ahead log encountered a read or write error"
 )
@@ -26,6 +29,32 @@ func (wal *WAL) Write(key int64, value string) error {
 	GenericWriteAheadLogError(err)
 	fmt.Printf("bytes written to WALog: '%d'\n", memsize)
 	return err
+}
+
+func (wal *WAL) Replay() ([]KeyValue, error) {
+	fsk, err := os.Open(wal.file.Name())
+	if err != nil {
+		fmt.Println(ErrOpeningFile)
+		return nil, errors.New(ErrOpeningFile)
+	}
+	defer fsk.Close()
+	var data []KeyValue
+	scanner := bufio.NewScanner(fsk)
+	for scanner.Scan() {
+		lines := scanner.Text()
+		var key int64
+		var value string
+		fmt.Sscanf(lines, "%d:%s", key, value)
+		data = append(data, KeyValue{key: key, value: value})
+	}
+	if err = scanner.Err(); err != nil {
+		return nil, errors.New(err.Error())
+	}
+	return data, nil
+}
+
+func (wal *WAL) Walclose() error {
+	return wal.file.Close()
 }
 
 func WriteAheadLogError(err error) {
