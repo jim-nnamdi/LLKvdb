048da5fe201981f778f0c8e9ad1241e70aba65c0
Jim <jimsamuel50@gmail.com>
2025-01-19 07:16:25 +0100

    WAL:fault tolerance as write ahead log before memtable implementations

--- pkg/model/wal.go
+++ pkg/model/wal.go
@@ -1,7 +1,43 @@
 package model
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	ErrFileProblems = "could not create or append data to files"
+	ErrGenericWAL   = "write ahead log encountered a read or write error"
+)
 
 type WAL struct {
 	file *os.File
+}
+
+func NewWAL(walLoc string) *WAL {
+	file, err := os.OpenFile(walLoc, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	WriteAheadLogError(err)
+	defer file.Close()
+	return &WAL{file: file}
+}
+
+func (wal *WAL) Write(key int64, value string) error {
+	memsize, err := wal.file.WriteString(fmt.Sprintf("%d:%s\n", key, value))
+	GenericWriteAheadLogError(err)
+	fmt.Printf("bytes written to WALog: '%d'\n", memsize)
+	return err
+}
+
+func WriteAheadLogError(err error) {
+	if err != nil {
+		fmt.Printf("WAL:'%s'\n", err)
+		fmt.Println(ErrFileProblems)
+	}
+}
+
+func GenericWriteAheadLogError(err error) {
+	if err != nil {
+		fmt.Printf("WAL:'%s'\n", err)
+		fmt.Println(ErrGenericWAL)
+	}
 }
