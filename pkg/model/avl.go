f5812b82688b85a5c4c11c5a6124e6f7b3c9cfff
Jim <jimsamuel50@gmail.com>
2025-01-22 20:59:08 +0100

    AVLTree : RotateLeft and RotateRight implementation on Tree

--- pkg/model/avl.go
+++ pkg/model/avl.go
@@ -1,1 +1,45 @@
 package model
+
+type AVLNode struct {
+	Key    int
+	Left   *AVLNode
+	Right  *AVLNode
+	Height uint
+}
+
+func NewAVLTree() *AVLNode {
+	return &AVLNode{
+		Key:    0,
+		Left:   nil,
+		Right:  nil,
+		Height: 0,
+	}
+}
+
+func (Node *AVLNode) max(a uint, b uint) uint {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func (Node *AVLNode) RotateRight(currentLeft *AVLNode) *AVLNode {
+	currentRight := currentLeft.Left
+	Lookupval := currentRight.Right
+	currentRight.Right = currentLeft
+	currentLeft.Left = Lookupval
+	currentLeft.Height = Node.max(currentLeft.Height, currentRight.Height) + 1
+	currentRight.Height = Node.max(currentLeft.Height, currentRight.Height) + 1
+	return currentRight
+}
+
+func (Node *AVLNode) RotateLeft(currentRight *AVLNode) *AVLNode {
+	currentLeft := currentRight.Left
+	Lookupval := currentLeft.Right
+	currentLeft.Left = currentRight
+	currentRight.Right = Lookupval
+	currentLeft.Height = Node.max(currentLeft.Height, currentRight.Height) + 1
+	currentRight.Height = Node.max(currentLeft.Height, currentRight.Height) + 1
+	return currentLeft
+}
